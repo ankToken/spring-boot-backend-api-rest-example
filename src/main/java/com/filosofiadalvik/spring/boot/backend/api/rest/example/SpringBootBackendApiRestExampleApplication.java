312a98a32dfc3c4cb58457b6fca5b5187a2c0dd5
Ricardo Cardona <ricardocardona.rc@gmail.com>
2020-07-28 12:58:31 -0500

    Feat: Added project initial setup Added SpringBoot project initial setup.

---
+++ src/main/java/com/filosofiadalvik/spring/boot/backend/api/rest/example/SpringBootBackendApiRestExampleApplication.java
@@ -0,0 +1,13 @@
+package com.filosofiadalvik.spring.boot.backend.api.rest.example;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringBootBackendApiRestExampleApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringBootBackendApiRestExampleApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/filosofiadalvik/spring/boot/backend/api/rest/example/SpringBootBackendApiRestExampleApplicationTests.java
@@ -0,0 +1,13 @@
+package com.filosofiadalvik.spring.boot.backend.api.rest.example;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringBootBackendApiRestExampleApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
