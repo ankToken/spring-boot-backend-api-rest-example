dfbea8006fbf6fa67e4991c68c4578fe120baae5
Ricardo Cardona <ricardocardona.rc@gmail.com>
2020-08-17 15:07:20 -0500

    feat/error-handling Added error handling in the backend

--- src/main/java/com/filosofiadalvik/spring/boot/backend/api/rest/example/models/entity/Client.java
+++ src/main/java/com/filosofiadalvik/spring/boot/backend/api/rest/example/models/entity/Client.java
@@ -23,11 +23,13 @@ public class Client implements Serializable {
 	@GeneratedValue(strategy = GenerationType.IDENTITY)
 	private Long id;
 
+	@Column(nullable = false)
 	private String name;
 
 	@Column(name = "last_name")
 	private String lastName;
 
+	@Column(unique = true)
 	private String email;
 
 	@Column(name = "create_at")
